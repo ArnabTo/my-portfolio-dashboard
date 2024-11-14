2b4e776f4a9315fd05383e998b0c5df66fd160ab
ArnabTo <arnabs929@gmail.com>
2024-11-14 23:45:19 +0600

    updated

--- src/model/ProjectModel.js
+++ src/model/ProjectModel.js
@@ -27,7 +27,7 @@ const projectSchema = new mongoose.Schema(
         type: String,
         required: true,
     },
-    technologies: {
+    techs: {
         type: [String],
         required: true,
     },
